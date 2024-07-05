e4c7280a5d4def127a6bc4966a019413b1e84bde
Ravi suthar <139873352+Ravi-FF@users.noreply.github.com>
2024-07-05 16:06:51 +0530

    React Js

--- React - Applying Redux/src/App.jsx
+++ React - Applying Redux/src/App.jsx
@@ -3,13 +3,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import FinanceManager from './Redux/FinanceManager/FinanceManager'
 import { Provider } from 'react-redux'
 import { store } from './Redux/app/store'
+import Todo from './Redux/Todo/Todo'
 
 export default function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path='/' Component={FinanceManager}></Route>
+          {/* <Route path='/' Component={FinanceManager}></Route> */}
+          <Route path='/' Component={Todo}></Route>
         </Routes>
       </BrowserRouter>
     </Provider>
--- React - Applying Redux/src/Redux/app/store.jsx
+++ React - Applying Redux/src/Redux/app/store.jsx
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import Counter from "../feature/Counter";
-// import scroe from "../feature/scroe";
-// import Todo from "../feature/Todo"
-// import Presonal_Expense from "./feature/finance"
 import Presonal_Expense from "../feature/finance"
+import todo from "../feature/todo";
 export const store = configureStore({
     reducer: {
-        preosnalExpesekey: Presonal_Expense
+        preosnalExpesekey: Presonal_Expense,
+        todokey: todo
     }
 })
